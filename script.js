8173c6863133fc62f85f6d8d1ebcbe78a444f051
jasonyeisley <117985534+jasonyeisley@users.noreply.github.com>
2022-12-05 18:55:42 -0500

    COMPLETE ERASE MODE
    - Complete the eraser mode

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
+// INITIAL SETUP
 const canvas = document.getElementById('canvas');
 const slider = document.getElementById('pixel-setter');
 const currentSize = document.getElementById('current-size')
 const modeCheckboxes = document.getElementsByClassName('mode-checkbox')
 slider.addEventListener('mousedown', sliderMove)
 slider.addEventListener('mouseup', createCanvas)
-let currentMode = draw
+let currentMode = 'draw'
 
+// SET THE DRAWING MODE FROM THE CHECKBOXES
 for (let checkbox of modeCheckboxes) {
     checkbox.addEventListener('click', setMode)
 };
 
 function setMode(e) {
-    //if e.target.checked equals true (didn't turn off all) then set the value
-    //make all boxes off and check this one
-    //else make the value off and no mode
-    let newMode = e.target.value
-    console.log(e.target.checked)
+
+    // UNCHECK ALL CHECKBOXES
+    for (let checkbox of modeCheckboxes) {
+        checkbox.checked = false
+    }
+    e.target.checked = true // CHECK ONLY THE CURRENT CHECKBOX
+    
+    currentMode = e.target.value
 }
 
-
+// CHANGE THE SIZE OF THE CANVAS WITH THE SLIDER
 function sliderMove(){
     const mouseSlider = slider.addEventListener('mousemove', (e) => {
         if (e.buttons == 1) {
@@ -27,6 +32,7 @@ function sliderMove(){
     slider.addEventListener('mouseup', () => slider.removeEventListener('mousemove', mouseSlider))
 }
 
+//CREATE THE NUMBER OF DIVS IN THE CANVAS
 function createCanvas() {
     canvas.replaceChildren()
     const dimension = slider.value
@@ -40,14 +46,34 @@ function createCanvas() {
         for (let k = 0; k < dimension; k++) {
             let pixel = document.createElement('div')
             pixel.className = 'pixel'
-            pixel.addEventListener('mouseenter', draw)
-            pixel.addEventListener('mousedown', draw)
+            pixel.addEventListener('mouseenter', mode)
+            pixel.addEventListener('mousedown', mode)
             row.appendChild(pixel)
         }
 
         canvas.appendChild(row)
     }
     currentSize.textContent = `${dimension}x${dimension}`
+
+    // When changing canvas size, reset mode to draw
+    currentMode = 'draw'
+    for (let checkbox of modeCheckboxes) {
+        if (checkbox.id == 'draw-mode') {
+            checkbox.checked = true
+        } else {
+            checkbox.checked = false
+        }
+    };
+}
+
+// CALL THE CORRECT DRAWING FUNCTION FROM THE CURRENT MODE CHECKBOX
+function mode(e) {
+    if (currentMode == 'draw') {
+        draw(e)
+    }
+    else if (currentMode == 'erase') {
+        erase(e)
+    }
 }
 
 function draw(e) {
